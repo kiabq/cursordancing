a1382b9e28c8d7055dda62de9878b8c5d911a237
kiabq <conway.joseph@pm.me>
2023-08-08 22:31:42 -0400

    feat: initial client socket code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("ws://localhost:3000");
+
+  // send a message to the server
+  socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
+  
+  // receive a message from the server
+  socket.on("hello from server", (...args) => {
+    console.log("hello")
+    // ...
+  });
+
   return (
     <div className="App">
       <header className="App-header">
