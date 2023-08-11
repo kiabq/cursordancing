04de811e5591a32b6c027bb75e59d7cdf36642f5
kiabq <conway.joseph@pm.me>
2023-08-11 16:25:01 -0400

    update: server to have a session that keeps track of sockets and socket positions

--- server/server.ts
+++ server/server.ts
@@ -10,16 +10,58 @@ const io = new Server(5173, {
         origin: "*",
         methods: ["GET", "POST"]
     }
-})
+});
+
+type ClientPosition = {
+  x: number,
+  y: number
+} | null;
+
+type ClientSocket = string | null;
+
+interface IClient {
+  [key: string]: {
+    socket: ClientSocket,
+    position: ClientPosition
+  },
+}
+
+class Clients {
+  clients: Partial<IClient>
+
+  constructor() {
+    this.clients = {};
+    this.saveClients = this.saveClients.bind(this);
+    this.removeClients = this.removeClients.bind(this);
+  }
+
+  public saveClients(socket: string, position: ClientPosition) {
+    this.clients[socket] = { socket: socket, position: position };
+  }
+
+  public removeClients(socket: string) {
+    delete this.clients[socket];
+  }
+}
+
+const session = new Clients();
 
 io.on("connection", (socket) => {
   socket.join("room");
+  session.saveClients(socket.id, null);
+
+  io.to("room").emit("clients", session.clients);
 
   socket.on("player_move", (...args) => {
-    io.to("room").emit("other_move", ...args, socket.id);
+    session.clients[socket.id]!.position = {x: args[0].x, y: args[0].y }
+
+    io.to("room").emit("other_move", ...args, socket.id, session.clients);
   })
 
   socket.on("disconnect", (reason) => {
+    session.removeClients(socket.id);
     console.log(`${socket.id} Disconnected: ${reason}`);
   })
+
+  console.log("Clients: ", session.clients);
 });
