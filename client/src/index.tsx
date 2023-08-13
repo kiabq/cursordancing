3e8e4f98d76f77b97fac1d2393311bebaa84327a
kiabq <conway.joseph@pm.me>
2023-08-13 02:48:31 -0400

    add: router for handling different pages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,37 @@
+//? Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { 
+  createBrowserRouter, 
+  RouterProvider 
+} from "react-router-dom";
+
+//? Components
 import App from './App';
+import CreateRoom from './pages/CreateRoom';
 import reportWebVitals from './reportWebVitals';
+
+//? Styling
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/room/:room",
+    element: <App />
+  },
+  {
+    path: '/create-room',
+    element: <CreateRoom />
+  }
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
